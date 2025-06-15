a07230f68f9c1ab6e2e69ff957c84b629eabfa26
Theo de Paula Silva <theodepaula2008@gmail.com>
2025-06-15 16:21:34 -0300

    verificação de email  melhorada e close bd

--- Planilha_Financeira.py
+++ Planilha_Financeira.py
@@ -1,27 +1,29 @@
 import pymysql
 import bcrypt
 import datetime
-
-conexaobd = pymysql.connect(
-    host='127.0.0.1',
-    user='root',
-    password='',
-    database='Planilha_Financeira'
-)
+from tkinter import *
 
 def cadastro():
+    conexaobd = pymysql.connect(
+    host='127.0.0.1',
+    user='root',
+    password='Noceutempao-123',
+    database='Planilha_Financeira'
+    )
     cursor = conexaobd.cursor()
     nome = input('Insira seu nome completo: ').strip()
     while  not all(c.isalpha() or c.isspace() for c in nome):
         nome = input('Nome inválido!! Insira novamente: ').strip()
-    email = input('Insira seu email: ')
-    cursor.execute('select email from usuario')
-    email_exist = cursor.fetchall()
-    if len(email_exist) != 0:
-        for i in email_exist:
-            while i == email:
-                email = input('O email ja existe!!\nTente novamente: ')
+    while True:
+        email = input('Insira seu email: ').strip()
+        cursor.execute('SELECT email FROM usuario WHERE email = %s', (email,))
+        if cursor.fetchone() is None:
+            break  # email ainda não existe → pode usar
+        else:
+            print('O email já existe! Tente outro.')
     senha = input('Insira uma senha: ')
+    while senha != input('Repita sua senha: '):
+        print('A senhas são diferentes!!')
     senha_hash = bcrypt.hashpw(senha.encode('utf-8'),bcrypt.gensalt())
     data_criacao = datetime.date.today()
     cod_id = 'select id from usuario order by id desc limit 1'
@@ -34,5 +36,6 @@ def cadastro():
     val = (id,nome,email,senha_hash,data_criacao)
     cursor.execute(cod_add,val)
     conexaobd.commit()
+    conexaobd.close()
 
 cadastro()
